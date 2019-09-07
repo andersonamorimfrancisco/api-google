48fba61b470537205477da1d8dc5f658035aa926
Anderson Amorim <user.anderson@gmail.com>
2019-09-07 13:26:16 -0300

    Refatorando

--- src/app.js
+++ src/app.js
@@ -1,55 +1,81 @@
+// Importando dependentas
+
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const cors = require("cors");
 
-function calcularDistancia(ponto1, ponto2) {
-  const distancia = Math.sqrt(
-    (ponto1.lng - ponto1.lat) * (ponto1.lng - ponto1.lat) +
-      (ponto2.lng - ponto2.lat) * (ponto2.lng - ponto2.lat)
-  );
-  console.log(distancia);
-  return { ponto1: ponto1.nome, ponto2: ponto2.nome, distancia: distancia };
+// Funções auxiliares
+
+async function calcularDistanciasEnderecos(listaEnderecos) {
+  const distancias = [];
+
+  for (let i = 0; i < listaEnderecos.length; i++) {
+    for (let j = i + 1; j < listaEnderecos.length; j++) {
+      distancias.push(calcularDistancia(listaEnderecos[i], listaEnderecos[j]));
+    }
+  }
+
+  return distancias;
 }
 
-async function pegardados(endereco) {
+async function fazerRequisicao(endereco) {
   const apiKey = "AIzaSyBDow-0L38r53OTg4sMeJrF-NQ7En5Qhc8";
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${endereco}&key=${apiKey}`;
-  const request = await axios.get(URL);
-  const requestData = await request.data;
+  const requisicao = await axios.get(URL);
+  const requisicaoDados = await requisicao.data;
   return {
-    nome: requestData.results[0].formatted_address,
-    lng: requestData.results[0].geometry.location.lng,
-    lat: requestData.results[0].geometry.location.lat
+    nome: requisicaoDados.results[0].formatted_address,
+    lng: requisicaoDados.results[0].geometry.location.lng,
+    lat: requisicaoDados.results[0].geometry.location.lat
+  };
+}
+
+async function buscarDadosEnderecos(listaEnderecos) {
+  const listaRespostas = [];
+
+  for (let i = 0; i < listaEnderecos.length; i++) {
+    listaRespostas.push(await fazerRequisicao(listaEnderecos[i]));
+  }
+
+  return listaRespostas;
+}
+
+function calcularDistancia(endereco1, endereco2) {
+  const distancia = Math.sqrt(
+    (endereco1.lng - endereco1.lat) * (endereco1.lng - endereco1.lat) +
+      (endereco2.lng - endereco2.lat) * (endereco2.lng - endereco2.lat)
+  );
+  return {
+    endereco1: endereco1.nome,
+    endereco2: endereco2.nome,
+    distancia: distancia
   };
 }
 
 const app = express();
+
+// Middlewares
+
 app.use(cors());
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+// Rotas
+
+app.get("/", (requisicao, resposta) => {
+  resposta.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.post("/request", async (req, res) => {
-  const { listaDeEnderecos } = req.body;
-  const enderecos = listaDeEnderecos.split(";");
-  const respostas = [];
+app.post("/calcular-distancias", async (requisicao, resposta) => {
+  const { enderecosAgrupados } = requisicao.body;
 
-  for (let i = 0; i < enderecos.length; i++) {
-    respostas.push(await pegardados(enderecos[i]));
-  }
+  const listaEnderecos = enderecosAgrupados.split(";");
+  const listaRespostas = await buscarDadosEnderecos(listaEnderecos);
+  const listaDistancias = await calcularDistanciasEnderecos(listaRespostas);
 
-  const distancias = [];
-
-  for (let i = 0; i < respostas.length; i++) {
-    for (let j = i + 1; j < respostas.length; j++) {
-      distancias.push(calcularDistancia(respostas[i], respostas[j]));
-    }
-  }
-
-  res.json(distancias);
+  resposta.json(listaDistancias);
 });
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT);
